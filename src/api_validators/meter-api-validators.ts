315c42dc20b6bdb8f54ea5c74610c89d4c785569
kadismile <ikadismile@gmail.com>
2024-08-03 13:53:06 -0500

    fixed error

--- src/api_validators/meter-api-validators.ts
+++ src/api_validators/meter-api-validators.ts
@@ -1,15 +1,15 @@
 import Joi from '@hapi/joi';
 
-import { METER_STATUS, METER_TYPE } from '../types/meter.js';
+import { METER_TYPE } from '../types/meter.js';
 
 export const createMeterApiValidator = Joi.object({
   meterNumber: Joi.string().required(),
   barcode: Joi.string().required(),
   typeOfMeter: Joi.string().valid(METER_TYPE).required(),
-  meterStatus: Joi.string().valid(METER_STATUS).required(),
+  meterStatus: Joi.string().required(),
   vendor: Joi.string().required()
 });
 
 export const updateMeterApiValidator = Joi.object({
-  meterStatus: Joi.string().valid(METER_STATUS).required()
+  meterStatus: Joi.string().required()
 });
