7e781b0362cc3786af4c5512f656f30674e9dce6
willitech <williamnnamani@yahoo.com>
2024-08-03 22:41:58 +0100

    change endpoint name

--- src/api_validators/meter-api-validators.ts
+++ src/api_validators/meter-api-validators.ts
@@ -5,11 +5,11 @@ import { METER_STATUS, METER_TYPE } from '../types/meter.js';
 export const createMeterApiValidator = Joi.object({
   meterNumber: Joi.string().required(),
   barcode: Joi.string().required(),
-  typeOfMeter: Joi.string().valid(METER_TYPE).required(),
-  meterStatus: Joi.string().valid(METER_STATUS).required(),
+  typeOfMeter: Joi.string().valid(...Object.values(METER_TYPE)).required(),
+  meterStatus: Joi.string().valid(...Object.values(METER_STATUS)).required(),
   vendor: Joi.string().required()
 });
 
 export const updateMeterApiValidator = Joi.object({
-  meterStatus: Joi.string().valid(METER_STATUS).required()
+  meterStatus: Joi.string().valid(...Object.values(METER_STATUS)).required()
 });
